a50e448ecbf875e805c85f501b2bdc5701e30347
Praneet0212 <71916363+Praneet0212@users.noreply.github.com>
2021-07-08 19:22:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+lipX = 0;
+lipY = 0
+
 function preload()
 {
+    moustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png')
 }
 
 function setup()
@@ -24,14 +28,17 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("lip x = " + results[0].pose.lip.x);
-        console.log("lip y = " + results[0].pose.lip.y);
+        lipX = results[0].pose.nose.x - 10; 
+        lipY = results[0].pose.nose.y - 15;
+        console.log("lip x = " + results[0].nose.lip.x);
+        console.log("lip y = " + results[0].nose.lip.y);
     }
 }
 
 function draw()
 {
     image(video, 0, 0, 300, 300);
+    image(moustache, lipX, lipY, 30, 30);
 }
 
 function take_snapshot()
